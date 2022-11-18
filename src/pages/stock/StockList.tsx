a1aa146dadbb1f02d778c2f3e188baadc3dbda98
YoungkeunAhn <tosan2094@gmail.com>
2022-11-18 17:03:09 +0900

    FIX: SearchOption, filter, 재고관리, 레이아웃 스타일

--- src/pages/stock/StockList.tsx
+++ src/pages/stock/StockList.tsx
@@ -1,22 +1,24 @@
 import axios from 'axios'
 import { map } from 'lodash'
+import { FilterMatchMode, FilterOperator } from 'primereact/api'
 import { Button } from 'primereact/button'
 import { Column } from 'primereact/column'
-import { DataTable } from 'primereact/datatable'
+import { DataTable, DataTableFilterMeta } from 'primereact/datatable'
 import { Dropdown, DropdownChangeParams } from 'primereact/dropdown'
 import { InputNumber } from 'primereact/inputnumber'
-import { SelectButton, SelectButtonChangeParams } from 'primereact/selectbutton'
 import { InputText } from 'primereact/inputtext'
+import { SelectButton, SelectButtonChangeParams } from 'primereact/selectbutton'
 import React, { useEffect, useState } from 'react'
 import { BASE_URL } from 'src/api/ApiConfig'
 import MenuButton from 'src/components/custom-buttons/MenuButton'
 import SearchCateTextOption from 'src/components/search-box/SearchCateTextOption'
 import SearchDateOption from 'src/components/search-box/SearchDateOption'
-import SearchMarketIdOption from 'src/components/search-box/SearchMarketIdOption'
 import { arrayCommaBodyTemplate, dateBodyTemplate, imageBodyTemplate, numberBodyTemplate } from 'src/hooks/data-table-hooks/BodyHooks'
 import { wrapColumnHeader } from 'src/hooks/data-table-hooks/HeaderHooks'
 import { ProductItemUnit } from 'src/types/product-manage'
 import LogViewModal from './LogViewModal'
+
+const globalFilterFields = ['skuName', 'managerName', 'skuId', 'barcode']
 
 type SearchOptions = {
     marketId: string
@@ -25,9 +27,9 @@ type SearchOptions = {
     startDate: string
     endDate: string
     searchText: string
-    searchCate: 'global' | 'productsName' | 'managerName' | 'products.0.items.0.units.0.skuId' | 'products.0.items.0.units.0.trade.purchasePrice'
+    searchCate: 'global' | 'skuName' | 'managerName' | 'skuId' | 'barcode'
 
-    searchNumberCate: string
+    searchRangeCate: 'stock.availableQty' | 'order.totalReceiptPrice' | 'order.lastReceiptPrice' | 'order.lastReceiptQty'
     searchNumberStart: number | null
     searchNumberEnd: number | null
 
@@ -43,7 +45,7 @@ const initSearhOptions: SearchOptions = {
     startDate: '',
     endDate: '',
 
-    searchNumberCate: 'stock.availableQty',
+    searchRangeCate: 'stock.availableQty',
     searchNumberStart: null,
     searchNumberEnd: null,
 
@@ -52,13 +54,13 @@ const initSearhOptions: SearchOptions = {
 
 const searchCateTextOptions = [
     { label: '통합', field: 'global' },
-    { label: '상품명', field: 'productsName' },
+    { label: '상품명', field: 'skuName' },
     { label: '담당자', field: 'managerName' },
     { label: '재고코드', field: 'skuId' },
     { label: '재고바코드', field: 'barcode' },
 ]
 
-const searchCateNumberOptions: SearchCate[] = [
+const searchRangeCateOptions: SearchCate[] = [
     { label: '재고수량', field: 'stock.availableQty' },
     { label: '재고금액', field: 'order.totalReceiptPrice' },
     { label: '최근입고가', field: 'order.lastReceiptPrice' },
@@ -70,10 +72,51 @@ const selectBtnOptions = [
     { name: '재고없음', value: 'emptyStock' },
 ]
 
+const initFileter: DataTableFilterMeta = {
+    global: { value: '', matchMode: FilterMatchMode.CONTAINS },
+    marketId: { value: '', matchMode: FilterMatchMode.CONTAINS },
+
+    skuName: { operator: FilterOperator.AND, constraints: [{ value: '', matchMode: FilterMatchMode.CONTAINS }] },
+    managerName: { value: '', matchMode: FilterMatchMode.CONTAINS },
+    skuId: { value: '', matchMode: FilterMatchMode.CONTAINS },
+    barcode: { value: '', matchMode: FilterMatchMode.CONTAINS },
+
+    'stock.availableQty': {
+        operator: FilterOperator.AND,
+        constraints: [
+            { value: 0, matchMode: FilterMatchMode.GREATER_THAN_OR_EQUAL_TO },
+            { value: 999999, matchMode: FilterMatchMode.LESS_THAN_OR_EQUAL_TO },
+        ],
+    },
+    'order.totalReceiptPrice': {
+        operator: FilterOperator.AND,
+        constraints: [
+            { value: 0, matchMode: FilterMatchMode.GREATER_THAN_OR_EQUAL_TO },
+            { value: 999999, matchMode: FilterMatchMode.LESS_THAN_OR_EQUAL_TO },
+        ],
+    },
+    'order.lastReceiptPrice': {
+        operator: FilterOperator.AND,
+        constraints: [
+            { value: 0, matchMode: FilterMatchMode.GREATER_THAN_OR_EQUAL_TO },
+            { value: 999999, matchMode: FilterMatchMode.LESS_THAN_OR_EQUAL_TO },
+        ],
+    },
+    'order.lastReceiptQty': {
+        operator: FilterOperator.AND,
+        constraints: [
+            { value: 0, matchMode: FilterMatchMode.GREATER_THAN_OR_EQUAL_TO },
+            { value: 999999, matchMode: FilterMatchMode.LESS_THAN_OR_EQUAL_TO },
+        ],
+    },
+}
+
 function StockList() {
     const [logId, setLogId] = useState<number>(0)
     const [unitList, setUnitList] = useState<ProductItemUnit[]>([])
     const [searchOptions, setSearchOptions] = useState<SearchOptions>(initSearhOptions)
+    const [selection, setSelection] = useState<ProductItemUnit[]>([])
+    const [filter, setFilter] = useState<DataTableFilterMeta>(initFileter)
 
     const [logOpen, setLogOpen] = useState<boolean>(false)
 
@@ -114,6 +157,52 @@ function StockList() {
         setLogId(0)
     }
 
+    const onSearch = () => {
+        const { searchCate, searchText, marketId, searchRangeCate, searchNumberStart, searchNumberEnd, serachIsStockQty, startDate, endDate } =
+            searchOptions
+
+        const globalFilter = {
+            operator: FilterOperator.OR,
+        }
+
+        const marketIdFilter = { value: marketId, matchMode: FilterMatchMode.CONTAINS }
+        const searchCateFilter = { value: searchText, matchMode: FilterMatchMode.CONTAINS }
+        const skuNameFilter = {
+            operator: FilterOperator.AND,
+            constraints: searchText.split(' ').map((word) => ({ value: word, matchMode: FilterMatchMode.CONTAINS })),
+        }
+        const rangeNumberFilter = {
+            operator: FilterOperator.AND,
+            constraints: [
+                { value: searchNumberStart || 0, matchMode: FilterMatchMode.GREATER_THAN_OR_EQUAL_TO },
+                { value: searchNumberEnd, matchMode: FilterMatchMode.LESS_THAN_OR_EQUAL_TO },
+            ],
+        }
+
+        if (searchCate === 'skuName') {
+            setFilter((prev) => ({
+                ...prev,
+                skuName: skuNameFilter,
+                [searchRangeCate]: rangeNumberFilter,
+
+                // marketId: marketIdFilter,
+            }))
+        } else {
+            setFilter((prev) => ({
+                ...prev,
+                [searchCate]: searchCateFilter,
+                [searchRangeCate]: rangeNumberFilter,
+
+                // marketId: marketIdFilter,
+            }))
+        }
+    }
+
+    const clearSearchOptions = () => {
+        setSearchOptions(initSearhOptions)
+        setFilter(initFileter)
+    }
+
     const getUnitList = async () => {
         try {
             const { data } = await axios.get(BASE_URL + 'units')
@@ -132,6 +221,10 @@ function StockList() {
         }
     }
 
+    const seqBodyTemplate = (rowData: any, options: any) => {
+        console.log(rowData, options)
+    }
+
     useEffect(() => {
         getUnitList()
     }, [])
@@ -148,7 +241,7 @@ function StockList() {
                         <span className="border rounded bg-white p-1 text-[11px] ml-4">Total : {unitList.length}</span>
                     </div>
                     <div className="flex space-x-4 px-4 pt-4">
-                        <SearchMarketIdOption value={searchOptions.marketId} onChange={onChangeSearchOptionDropdown} />
+                        {/* <SearchMarketIdOption value={searchOptions.marketId} onChange={onChangeSearchOptionDropdown} /> */}
                         <SearchCateTextOption
                             cate={searchOptions.searchCate}
                             text={searchOptions.searchText}
@@ -159,16 +252,25 @@ function StockList() {
                         <div className="flex space-x-2 items-center">
                             <Dropdown
                                 className="min-w-[100px]"
-                                name="searchNumberCate"
+                                name="searchRangeCate"
                                 optionLabel="label"
                                 optionValue="field"
-                                options={searchCateNumberOptions}
-                                value={searchOptions.searchNumberCate}
+                                options={searchRangeCateOptions}
+                                value={searchOptions.searchRangeCate}
+                                onChange={onChangeSearchOptionDropdown}
                             />
                             <div className="flex space-x-2 items-center">
-                                <InputNumber value={searchOptions.searchNumberStart} />
+                                <InputNumber
+                                    name="searchNumberStart"
+                                    value={searchOptions.searchNumberStart}
+                                    onChange={(e) => setSearchOptions((prev) => ({ ...prev, searchNumberStart: e.value }))}
+                                />
                                 <span>~</span>
-                                <InputNumber value={searchOptions.searchNumberEnd} />
+                                <InputNumber
+                                    name="searchNumberEnd"
+                                    value={searchOptions.searchNumberEnd}
+                                    onChange={(e) => setSearchOptions((prev) => ({ ...prev, searchNumberEnd: e.value }))}
+                                />
                             </div>
                         </div>
                     </div>
@@ -189,15 +291,19 @@ function StockList() {
                 <div className="border-l flex flex-col">
                     <div className="h-[65px]"></div>
                     <div className="flex items-end space-x-2 p-4">
-                        <button className="btn default-btn">초기화</button>
-                        <button className="btn primary-btn">검색</button>
+                        <button className="btn default-btn" onClick={clearSearchOptions}>
+                            초기화
+                        </button>
+                        <button className="btn primary-btn" onClick={onSearch}>
+                            검색
+                        </button>
                     </div>
                 </div>
             </div>
             <div className="card">
                 <div className="flex items-center justify-between pb-4">
                     <div className="flex space-x-2 items-center">
-                        <span className="font-bold">수동입출고</span>
+                        <span className="font-bold">재고수동입출고</span>
                         <InputText placeholder="상세내역" className="w-[300px] h-[30px] rounded" />
                         <InputText placeholder="수량" className="w-[80px] h-[30px] rounded text-center" />
                         <button className="min-w-[50px] h-[30px] border border-[#146BCE] text-[#146BCE] rounded text-sm font-bold">입고</button>
@@ -220,79 +326,84 @@ function StockList() {
                         >
                             전체로그확인
                         </button>
-                        <button className="font-bold border text-sm border-[#C6D4E6] rounded text-black p-1 px-2 bg-[#F5F9FD] h-[32px]">
+                        {/* <button className="font-bold border text-sm border-[#C6D4E6] rounded text-black p-1 px-2 bg-[#F5F9FD] h-[32px]">
                             그리드 항목설정
-                        </button>
+                        </button> */}
                     </div>
                 </div>
-                <div className="flex flex-col">
-                    <DataTable
-                        value={unitList}
-                        rows={15}
-                        paginator
-                        removableSort
-                        scrollable
-                        scrollHeight="70vh"
-                        sortMode="multiple"
-                        responsiveLayout="scroll"
-                        columnResizeMode="expand"
-                    >
-                        <Column align="center" selectionMode="multiple" selectionAriaLabel="id" headerClassName="w-[10px]" field="id"></Column>
-                        <Column align="center" className="text-[12px]" field="seq" header="NO" />
-                        <Column align="center" className="text-[12px]" field="managerName" header="담당자" />
-                        <Column align="center" className="text-[12px]" field="skuId" header="재고코드" />
-                        <Column align="center" className="text-[12px]" field="image" header="이미지" body={imageBodyTemplate} />
-                        <Column
-                            alignHeader="center"
-                            align="left"
-                            className="text-[12px] w-[300px]"
-                            headerClassName="w-[300px]"
-                            field="skuName"
-                            header="상품명"
-                        />
-                        <Column
-                            align="center"
-                            className="text-[12px]"
-                            field="order.lastReceiptPrice"
-                            header={wrapColumnHeader('최근 입고가격')}
-                            body={(rowData) => numberBodyTemplate(rowData.order.lastReceiptPrice)}
-                            sortable
-                        />
-                        <Column
-                            align="center"
-                            className="text-[12px]"
-                            field="order.lastReceiptQty"
-                            header={wrapColumnHeader('최근 입고수량')}
-                            body={(rowData) => numberBodyTemplate(rowData.order.lastReceiptQty)}
-                        />
-                        <Column
-                            align="center"
-                            className="text-[12px]"
-                            field="order.lastReceiptDate"
-                            header={wrapColumnHeader('최근 입고일')}
-                            body={dateBodyTemplate}
-                            sortable
-                        />
-                        <Column
-                            align="center"
-                            className="text-[12px]"
-                            field="order.totalReceiptPrice"
-                            header={wrapColumnHeader('재고금액 (수량x입고가격)')}
-                            body={(rowData) => numberBodyTemplate(rowData.order.totalReceiptPrice)}
-                        />
-                        <Column align="center" className="text-[12px]" field="stock.availableQty" header="재고수량" sortable />
-                        <Column align="center" className="text-[12px]" field="stock.disusedQty" header="불량수량" sortable />
-                        <Column
-                            align="center"
-                            className="text-[12px]"
-                            field="stock.locations"
-                            header="파렛트위치"
-                            body={(rowData) => arrayCommaBodyTemplate(rowData.stock.locations)}
-                        />
 
-                        <Column align="center" className="text-[12px]" field="log" header="로그" body={logBodyTemplate} />
-                    </DataTable>
-                </div>
+                <DataTable
+                    value={unitList}
+                    removableSort
+                    rows={5}
+                    scrollHeight="82vh"
+                    sortMode="multiple"
+                    responsiveLayout="scroll"
+                    selection={selection}
+                    onSelectionChange={(e) => setSelection(e.value)}
+                    filters={filter}
+                    globalFilterFields={globalFilterFields}
+                    onValueChange={(e) => console.log('onValueChange : ', e)}
+                    // resizableColumns
+                    // columnResizeMode="expand"
+                >
+                    <Column align="center" className="max-w-[50px]" selectionMode="multiple" selectionAriaLabel="id" field="id"></Column>
+                    <Column align="center" className="text-[12px]" field="seq" header="NO" bodyStyle={seqBodyTemplate} />
+                    <Column align="center" className="text-[12px]" field="managerName" header="담당자" />
+                    <Column align="center" className="text-[12px]" field="skuId" header="재고코드" />
+                    <Column align="center" className="text-[12px] p-0 " field="image" header="이미지" body={imageBodyTemplate} />
+                    <Column
+                        alignHeader="center"
+                        align="left"
+                        className="text-[12px] min-w-[300px]"
+                        headerClassName="min-w-[300px]"
+                        field="skuName"
+                        filter
+                        filterField="skuName"
+                        header="상품명"
+                    />
+                    <Column
+                        align="center"
+                        className="text-[12px]"
+                        field="order.lastReceiptPrice"
+                        header={wrapColumnHeader('최근 입고가격')}
+                        body={(rowData) => numberBodyTemplate(rowData.order.lastReceiptPrice)}
+                        sortable
+                    />
+                    <Column
+                        align="center"
+                        className="text-[12px]"
+                        field="order.lastReceiptQty"
+                        header={wrapColumnHeader('최근 입고수량')}
+                        body={(rowData) => numberBodyTemplate(rowData.order.lastReceiptQty)}
+                    />
+                    <Column
+                        align="center"
+                        className="text-[12px]"
+                        field="order.lastReceiptDate"
+                        header={wrapColumnHeader('최근 입고일')}
+                        body={dateBodyTemplate}
+                        sortable
+                    />
+                    <Column
+                        align="center"
+                        className="text-[12px]"
+                        field="order.totalReceiptPrice"
+                        header={wrapColumnHeader('재고금액 (수량x입고가격)')}
+                        body={(rowData) => numberBodyTemplate(rowData.order.totalReceiptPrice)}
+                    />
+                    <Column align="center" className="text-[12px]" field="stock.availableQty" header="재고수량" sortable />
+                    <Column align="center" className="text-[12px]" field="stock.disusedQty" header="불량수량" sortable />
+                    <Column
+                        align="center"
+                        className="text-[12px]"
+                        field="stock.locations"
+                        header="파렛트위치"
+                        body={(rowData) => arrayCommaBodyTemplate(rowData.stock.locations)}
+                    />
+
+                    <Column align="center" className="text-[12px]" field="log" header="로그" body={logBodyTemplate} />
+                </DataTable>
             </div>
             <LogViewModal open={logOpen} onClose={closeLogModal} logId={logId} />
         </div>
